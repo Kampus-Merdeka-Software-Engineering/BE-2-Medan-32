d64b215e410b6b99627cb98ac8444e00aa8403d8
unknown <bayuara168@gmail.com>
2023-12-07 22:16:18 +0700

    change port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const path = require('path');
-const cors = require('cors');
-const PORT = process.env.PORT || 4000;
+const path = require("path");
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const newsRouter = require('./src/routes/news');
+const newsRouter = require("./src/routes/news");
 const commentRouter = require("./src/routes/comments");
 const dbConnection = require("./src/config/database");
 // const usersRouter = require("./src/routes/users");
@@ -13,21 +13,22 @@ const dbConnection = require("./src/config/database");
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.use(newsRouter);
 // app.use(commentRouter);
 // app.use(usersRouter);
 
-dbConnection.sync({ alter: true })
-.then(()=> {
-  console.log("Database connected");
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
+dbConnection
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Database connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port http://localhost:${PORT}`);
+    });
   })
-})
-.catch(() => console.log(`Unable to connect to databse: ${error}`));
+  .catch(() => console.log(`Unable to connect to databse: ${error}`));
 // "0.0.0.0"
