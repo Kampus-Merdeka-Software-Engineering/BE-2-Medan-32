95c35d06ee7ed2588672fb3d522ac8ed816cc04b
unknown <bayuara168@gmail.com>
2023-12-07 22:43:32 +0700

    fix: error port

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,15 @@
 const sequelize = require("sequelize");
 require("dotenv").config();
 
-const dbConnection = new sequelize.Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-});
+const dbConnection = new sequelize.Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+  }
+);
 
 module.exports = dbConnection;
