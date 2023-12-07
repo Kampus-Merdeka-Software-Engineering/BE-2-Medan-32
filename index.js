93162f2c95336d3b0fde2015d0789f1b6ebbe3bd
unknown <bayuara168@gmail.com>
2023-12-07 22:25:50 +0700

    error handling on main index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 
 const newsRouter = require("./src/routes/news");
 const commentRouter = require("./src/routes/comments");
@@ -11,15 +13,19 @@ const dbConnection = require("./src/config/database");
 // const usersRouter = require("./src/routes/users");
 
 app.use(express.json());
-app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.send("Hello World!");
 });
 
 app.use(newsRouter);
 // app.use(commentRouter);
 // app.use(usersRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 dbConnection
   .sync({ alter: true })
@@ -30,5 +36,5 @@ dbConnection
       console.log(`Server running on port http://localhost:${PORT}`);
     });
   })
-  .catch(() => console.log(`Unable to connect to databse: ${error}`));
+  .catch((error) => console.log(`Unable to connect to databse: ${error}`));
 // "0.0.0.0"
